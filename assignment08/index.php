46d30f2eb5255338c12e850ed7fa70a50888a770
emthoendel <54589936+emthoendel@users.noreply.github.com>
2020-02-17 13:15:08 -0600

    zoom session

--- assignment08/index.php
+++ assignment08/index.php
@@ -1,24 +1,24 @@
 
 <?php
     include "includes/header.php";
-    $my_weight= '125 lb';
+    $my_weight= 125;
     $_age = 25;
     $first_name='Donjeta';
     $last_name='Emerllahu';
-    $poundsToKg= 'pounds *(1.2.2)';
+    //$poundsToKg= 'pounds *(1.2.2)';
     //write PHP here...
-   function poundsToKg($my_weight){
-       $my_weight = ("pounds *(1/2.2");
-       return $my_weight;
+   function poundsToKg($w){
+       $w = ($w *(1/2.2));
+       return $w;
     
    }
-   $result = add ($my_weight + $poundsToKg)
+   $result = poundsToKg($my_weight);
 
 ?>
 <body>
 <p>Good morning, ladies and gentlemen!</p>
 <p>My name is <?php Echo $first_name;?> <?php Echo $last_name;?>!
-<p>I am <?php Echo $_age;?> years old. I weigh <?php Echo $result;?>pounds, which is <?php Echo $my_weight;?> kg!</p>
+<p>I am <?php Echo $_age;?> years old. I weigh <?php Echo $my_weight;?>pounds, which is <?php Echo $result;?> kg!</p>
 
 </body>
 <?php
